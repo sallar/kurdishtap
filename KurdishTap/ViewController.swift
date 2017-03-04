e0eb65985c402b9b963c6926cb70c1d5eb03b14a
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-03-04 12:20:21 +0200

    Initial Commit

---
+++ KurdishTap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KurdishTap
+//
+//  Created by Sallar Kaboli on 04/03/2017.
+//  Copyright © 2017 Sallar Kaboli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
